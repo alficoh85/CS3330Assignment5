6db65056002dd9054f3eb56349a45557fcba64d5
Alex Cohen <alficoh85@gmail.com>
2025-05-04 21:28:57 -0500

    Finished code for the Comparators and added Javadoc

--- src/main/java/com/adoptme/model/SpeciesComparator.java
+++ src/main/java/com/adoptme/model/SpeciesComparator.java
@@ -1,5 +1,11 @@
 package main.java.com.adoptme.model;
 
-public class SpeciesComparator {
+import java.util.Comparator;
 
+/** The AgeComparator class is used to make comparisons using the species of Pet objects. */
+public class SpeciesComparator implements Comparator<Pet>{
+    @Override
+    public int compare(Pet pet1, Pet pet2) {
+        return pet1.species.compareTo(pet2.species);
+    }
 }
