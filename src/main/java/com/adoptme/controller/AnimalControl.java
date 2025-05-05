4ac6f38504207ae415edb96976948799c6176370
WoodySmith2004 <bsmith_04@yahoo.com>
2025-05-04 21:45:48 -0500

    Establish init and the functions of Controller

--- src/main/java/com/adoptme/controller/AnimalControl.java
+++ src/main/java/com/adoptme/controller/AnimalControl.java
@@ -1,5 +1,46 @@
 package main.java.com.adoptme.controller;
 
-public class AnimalControl {
+import main.java.com.adoptme.model.Pet;
+import main.java.com.adoptme.model.Shelter;
+import main.java.com.adoptme.view.View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+public class AnimalControl {
+	private final Shelter<Pet> model;
+	private final View view;
+	
+	public AnimalControl(Shelter<Pet> model, View view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void initController() {
+		refreshPetTable();
+		view.getAddButton().addActionListener(e -> onAddPet());
+		view.getAdoptButton().addActionListener(e -> onAdoptPet());
+		view.getSortComboBox().addActionListener(e -> onSortSelection());
+		view.getSaveButton().addActionListener(e -> onSave());
+	}
+	
+	private void onAddPet() {
+		
+	}
+	
+	private void onAdoptPet() {
+		
+	}
+	
+	private void onSortSelection() {
+		
+	}
+	
+	private void onSave() {
+		
+	}
+	
+	private void refreshPetTable() {
+		
+	}
 }
