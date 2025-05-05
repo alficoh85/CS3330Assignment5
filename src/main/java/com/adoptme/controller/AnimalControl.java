38ffd7782a2eaa31a6e12e69d5d865fcd825f54c
Alex Cohen <alficoh85@gmail.com>
2025-05-04 21:36:22 -0500

    Removed code for segment I am not doing

--- src/main/java/com/adoptme/controller/AnimalControl.java
+++ src/main/java/com/adoptme/controller/AnimalControl.java
@@ -4,13 +4,5 @@ import main.java.com.adoptme.model.Pet;
 import main.java.com.adoptme.model.Shelter;
 
 public class AnimalControl {
-	private Shelter<Pet> shelter;
 	
-	public boolean adopt(Pet pet) {
-		if (pet.isAdopted() == false) {
-			pet.setAdopted(true);
-			return true;
-		}
-		return false;
-	}
 }
