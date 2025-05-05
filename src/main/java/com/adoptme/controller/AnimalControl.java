b9bcb65a6b7fff8585898e76c56208da28795971
WoodySmith2004 <bsmith_04@yahoo.com>
2025-05-04 22:29:22 -0500

    actionevent removal

--- src/main/java/com/adoptme/controller/AnimalControl.java
+++ src/main/java/com/adoptme/controller/AnimalControl.java
@@ -3,7 +3,6 @@ package main.java.com.adoptme.controller;
 import main.java.com.adoptme.model.Pet;
 import main.java.com.adoptme.model.Shelter;
 import main.java.com.adoptme.view.View;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Comparator;
