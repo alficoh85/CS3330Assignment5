cffc0f599e78619307979a991efc497f175cac37
Alex Cohen <alficoh85@gmail.com>
2025-05-04 20:18:49 -0500

    Added getters and setters for Pet's type variable, added Javadoc to Pet

--- src/main/java/com/adoptme/Main.java
+++ src/main/java/com/adoptme/Main.java
@@ -1,7 +1,5 @@
 package main.java.com.adoptme;
 
-import main.java.com.adoptme.model.*;
-
 public class Main {
 	public static void main(String[] args) {
 		//TODO code goes here
--- src/main/java/com/adoptme/model/Pet.java
+++ src/main/java/com/adoptme/model/Pet.java
@@ -9,6 +9,13 @@ public abstract class Pet {
     protected int age;
     protected boolean adopted;
     
+    /** Parameterized constructor, creates a new Pet object
+     * @param id - The ID of the pet
+     * @param name - The name of the pet
+     * @param type - The specific animal the pet is (cat, dog, rabbit, etc.)
+     * @param species - The specific species of the pet (Golden Retriever, Siamese, German Shepherd, etc.)
+     * @param age - How many years old the pet is
+     * @param adopted - Whether or not the pet has been adopted */
 	public Pet(int id, String name, String type, String species, int age, boolean adopted) {
 		this.id = id;
 		this.name = name;
@@ -18,42 +25,74 @@ public abstract class Pet {
 		this.adopted = adopted;
 	}
 
+	/** Accessor method for id
+	 * @return The pet's ID */
 	public int getId() {
 		return id;
 	}
 	
+	/** Mutator method for id, sets a pet's ID
+	 * @param id - The new ID to set the pet to */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/** Accessor method for name
+	 * @return The pet's name */
 	public String getName() {
 		return name;
 	}
 	
+	/** Mutator method for name, sets a pet's name
+	 * @param name - The name to set the pet to */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/** Accessor method for type
+	 * @return The type of pet the pet is */
+	public String getType() {
+		return type;
+	}
+
+	/** Mutator method for type
+	 * @param type - The type of pet to set the pet to */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/** Accessor method for species
+	 * @return The species the pet is set as */
 	public String getSpecies() {
 		return species;
 	}
 	
+	/** Mutator method for species, sets a pet's species
+	 * @param species - The species to set the pet as */
 	public void setSpecies(String species) {
 		this.species = species;
 	}
 	
+	/** Accessor method for age
+	 * @return The age of the pet */
 	public int getAge() {
 		return age;
 	}
 	
+	/** Mutator method for age
+	 * @param age - The age to set the pet as */
 	public void setAge(int age) {
 		this.age = age;
 	}
 	
+	/** Accessor method for adopted
+	 * @return Whether or not the pet is adopted */
 	public boolean isAdopted() {
 		return adopted;
 	}
 	
+	/** Mutator method for adopted, sets a pet's adopted status
+	 * @param adopted - true if the pet has been adopted, false if it hasn't */
 	public void setAdopted(boolean adopted) {
 		this.adopted = adopted;
 	}
